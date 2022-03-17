d9ad7575cf1a0b8633eb54c9370b348448f289c9
AlessioGaro <83210126+AlessioGaro@users.noreply.github.com>
2022-03-17 14:49:07 +0100

    Delete gestione-candidature-stage directory

--- gestione-candidature-stage/src/main/java/com/alessio/gestioneCandidatureStage/config/SecurityWebApplicationInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.alessio.gestioneCandidatureStage.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
